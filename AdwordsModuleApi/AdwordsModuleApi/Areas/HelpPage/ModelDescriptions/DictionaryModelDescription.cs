6688dbc90e0fc983c294d6f697a04daaadaa59fd
ste@mcb.dk <stefan@nolleren.org>
2017-11-02 08:38:03 +0100

    First commit

---
+++ AdwordsModuleApi/AdwordsModuleApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AdwordsModuleApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ AdwordsModuleApi/AdwordsModuleApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AdwordsModuleApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
