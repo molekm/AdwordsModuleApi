69171881c4b49f30dc8b5188f2731e29d6f81472
ste@mcb.dk <stefan@nolleren.org>
2017-11-02 10:02:49 +0100

    First Adwords Api test

--- AdwordsModuleApi/AdwordsModuleApi/Controllers/Api/HelloController.cs
+++ AdwordsModuleApi/AdwordsModuleApi/Controllers/Api/HelloController.cs
@@ -5,6 +5,8 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Script.Serialization;
+using Google.Api.Ads.AdWords.Lib;
+using Google.Api.Ads.AdWords.v201710;
 
 namespace AdwordsModuleApi.Controllers.Api
 {
@@ -14,18 +16,52 @@ namespace AdwordsModuleApi.Controllers.Api
         [HttpGet]
         public IHttpActionResult SayHello()
         {
-            Person person = new Person
+            CampaignPage page = new CampaignPage();
+
+            page = Run(new AdWordsUser());
+
+            return Ok(page.entries);
+        }
+
+        public CampaignPage Run(AdWordsUser user)
+        {
+            using (CampaignService campaignService =
+                (CampaignService)user.GetService(AdWordsService.v201710.CampaignService))
             {
-                name = "Stefan",
-                address = "Vesterled 47",
-                age = 35
-            };
 
-            //var json = new JavaScriptSerializer().Serialize(person);
+                // Create the selector.
+                Selector selector = new Selector()
+                {
+                    fields = new string[] {
+                 Campaign.Fields.Id, Campaign.Fields.Name, Campaign.Fields.Labels, Campaign.Fields.Status
+                 //},
+                 //   predicates = new Predicate[] {
+          // Labels filtering is performed by ID. You can use CONTAINS_ANY to
+          // select campaigns with any of the label IDs, CONTAINS_ALL to select
+          // campaigns with all of the label IDs, or CONTAINS_NONE to select
+          // campaigns with none of the label IDs.
+          //Predicate.ContainsAny(Campaign.Fields.Labels, new string[] { labelId.ToString() })
+        },
+                    paging = Paging.Default
+                };
 
-            return Ok(person);
+                CampaignPage page = new CampaignPage();
+
+                try
+                {
+                    // Get the campaigns.
+                    page = campaignService.get(selector);
+                }
+                catch (Exception e)
+                {
+                    throw new System.ApplicationException("Failed to retrieve campaigns by label", e);
+                }
+                return page;
+            }
         }
     }
+}
+
 
     public class Person
     {
@@ -33,4 +69,4 @@ namespace AdwordsModuleApi.Controllers.Api
         public string address;
         public int age;
     }
-}
+
