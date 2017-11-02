c5d01257f2fa59c4a37d47134f1e43a2626d79ab
ste@mcb.dk <stefan@nolleren.org>
2017-11-02 09:22:05 +0100

    Hello Test

---
+++ AdwordsModuleApi/AdwordsModuleApi/Controllers/Api/HelloController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Script.Serialization;
+
+namespace AdwordsModuleApi.Controllers.Api
+{
+    [Route("api/hello")]
+    public class HelloController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult SayHello()
+        {
+            Person person = new Person
+            {
+                name = "Stefan",
+                address = "Vesterled 47",
+                age = 35
+            };
+
+            var json = new JavaScriptSerializer().Serialize(person);
+
+            return Ok(json);
+        }
+    }
+
+    public class Person
+    {
+        public string name;
+        public string address;
+        public int age;
+    }
+}
